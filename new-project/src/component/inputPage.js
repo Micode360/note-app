dca4a2b9c14dacdfc6e56244de1900d36e7b0370
KING <abayemiracle@gmail.com>
2021-03-07 22:50:10 +0100

    text editor update

--- new-project/src/component/inputPage.js
+++ new-project/src/component/inputPage.js
@@ -1,10 +1,37 @@
 import React,{ useState, setState, useEffect } from 'react'
 import HeaderContainer from './header'
 import axios from 'axios'
+import DOMPurify from 'dompurify';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { Container, Form, Col } from 'react-bootstrap'
 
 
 const BlogInputContainer = () => {
+
+        const [editorState, setEditorState] = useState(
+                () => EditorState.createEmpty(),
+              );
+
+              const  [convertedContent, setConvertedContent] = useState(null);
+
+              const handleEditorChange = (state) => {
+                setEditorState(state);
+                convertContentToHTML();
+              }
+
+              const convertContentToHTML = () => {
+                let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+                setConvertedContent(currentContentAsHTML);
+              }
+
+              const createMarkup = (html) => {
+                return  {
+                  __html: DOMPurify.sanitize(html)
+                }
+              }
 
             return(
                     <>
@@ -13,13 +40,19 @@ const BlogInputContainer = () => {
                     <Container>
                             <Col className="form-container">
                                  <Form>
-                                         <button className="button">Post</button>
+                                        <Editor
+                                                editorState={editorState}
+                                                onEditorStateChange={handleEditorChange}
+                                                wrapperClassName="wrapper-class"
+                                                editorClassName="editor-class"
+                                                toolbarClassName="toolbar-class"
+                                        />
+
+                                <button className="button">Create Blog</button>
                                  </Form>
                             </Col>
-
-                            <Col className="output">
-        
-                            </Col>
+                            <Col className="output" dangerouslySetInnerHTML={createMarkup(convertedContent)}></Col>
+                            
 
                             
                     </Container>
